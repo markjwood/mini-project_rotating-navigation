9ee200d19ff28a5843b8f38ef609d8f6c8d26b4d
Mark <mark@markjwood.com>
2021-09-09 07:44:37 -0700

    fixed issue where nav menu wasn't clickable, by raising its z-index when the hamburger button is clicked

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const circle = document.getElementById('circle');
 const hambBtn = document.getElementById('hambBtn');
 const closeBtn = document.getElementById('closeBtn');
 const mainContent = document.querySelector('main');
+const navMenu = document.querySelector('nav.main-nav');
 const navLinks = document.querySelector('.main-nav--links');
 const navLis = document.querySelectorAll('.main-nav--links > li');
 
@@ -10,11 +11,15 @@ hambBtn.addEventListener('click', () => {
 	mainContent.classList.add('rotate');
   navLinks.classList.add('skew');
   navLis.forEach(li => li.classList.add('unskew'));
+  setTimeout(() => {
+    navMenu.classList.add('above');
+  }, 200);
 });
 
 closeBtn.addEventListener('click', () => {
 	circle.classList.remove('rotate');
 	mainContent.classList.remove('rotate');
-  navLinks.classList.remove('skew');
   navLis.forEach(li => li.classList.remove('unskew'));
+  navLinks.classList.remove('skew');
+  navMenu.classList.remove('above');
 });
